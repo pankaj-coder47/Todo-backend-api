3082d092ab417dd83a6fc22c8ca082eb7995e54a
dhanji-kumar <kdhanji183@gmail.com>
2023-09-16 10:12:36 +0530

    final commit for server deployement

--- auth/authenticator.js
+++ auth/authenticator.js
@@ -5,14 +5,14 @@ const erroHandler = require('../utils/error1')
 const isAuthenticated = async (req, res, next) => {
     try {
         const { token } = req.cookies;
-    if (!token) return next (new erroHandler("login first",400))
-    const decoded = jwt.decode(token, process.env.JWT_SECRET_KEY)
-    req.user = await User.findById(decoded._id)
-    next()
+        if (!token) return next(new erroHandler("login first", 400))
+        const decoded = jwt.decode(token, process.env.JWT_SECRET_KEY)
+        req.user = await User.findById(decoded._id)
+        next()
     } catch (error) {
         console.log("Error throw in IsAuthenticated : " + error)
     }
-    
+
 }
 
 module.exports = isAuthenticated;
